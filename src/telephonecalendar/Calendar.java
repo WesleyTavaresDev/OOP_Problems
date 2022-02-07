9c47266775c7978d3d027dca2620385180f8ce32
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-07 12:50:46 -0300

    Telephone Calendar:
    Contact class -> formed by two fields, name and phoneNumber that will form a hashmap in Calendar class

---
+++ src/telephonecalendar/Calendar.java
@@ -0,0 +1,5 @@
+package telephonecalendar;
+
+public class Calendar {
+
+}
---
+++ src/telephonecalendar/Contact.java
@@ -0,0 +1,13 @@
+package telephonecalendar;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Contact {
+    private String name;
+    private int phoneNumber;
+}
