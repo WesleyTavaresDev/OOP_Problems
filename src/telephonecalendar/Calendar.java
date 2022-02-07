a1cf9e3a1bafeab5c0d320ae7dada01dc38bd88a
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-07 13:38:50 -0300

    Telephone Calendar:
    Extract function -> all three contact objects moved to a method that returns a list

--- src/telephonecalendar/Calendar.java
+++ src/telephonecalendar/Calendar.java
@@ -5,15 +5,21 @@ import java.util.List;
 
 public class Calendar {
     public static void main(String[] args) {
+        Calendar phoneCalendar = new Calendar();
 
+        List<Contact> contact = phoneCalendar.contacts();
+
+        for (Contact c : contact)
+            System.out.printf("Name -> %s%nPhone Number -> %s%n", c.getName(), c.getPhoneNumber());
+    }
+
+    List<Contact> contacts()
+    {
         Contact brenda = new Contact("Brenda", 98560584L);
         Contact wesley = new Contact("Wesley", 98734096L);
         Contact martin = new Contact("Beatriz", 92134609L);
 
-        List<Contact> contact = Arrays.asList(brenda, wesley, martin);
-
-        for (Contact c : contact)
-            System.out.printf("Name -> %s%nPhone Number -> %s%n", c.getName(), c.getPhoneNumber());
+        return  Arrays.asList(brenda, wesley, martin);
     }
 
 }
